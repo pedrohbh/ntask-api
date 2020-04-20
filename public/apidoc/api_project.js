1e830c5e4ed571f8077b23fead8a0d9cc80a917e
Pedro Hoppe <pedrohbh@github.com>
2020-04-20 20:08:48 -0300

    Fim da seção 10.1

---
+++ public/apidoc/api_project.js
@@ -0,0 +1,17 @@
+define({
+  "name": "Documentação - Node Task API",
+  "template": {
+    "forceLanguage": "pt_br"
+  },
+  "version": "1.0.0",
+  "description": "API de gestão de tarefas",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-04-20T23:07:39.281Z",
+    "url": "http://apidocjs.com",
+    "version": "0.20.1"
+  }
+});
